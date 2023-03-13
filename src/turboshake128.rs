1c9a275589703582494a9acad80b391f3aa412e8
Anjan Roy <hello@itzmeanjan.in>
2023-03-13 16:43:37 +0400

    squeeze arbitrary bytes out of sponge state
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/turboshake128.rs
+++ src/turboshake128.rs
@@ -1,4 +1,5 @@
 use crate::keccak;
+use std::cmp;
 
 /// TurboSHAKE128 Extendable Output Function (XOF)
 ///
@@ -24,15 +25,15 @@ impl TurboShake128 {
         Self {
             state: [0u64; 25],
             offset: 0,
-            is_ready: 0,
+            is_ready: usize::MIN,
             squeezable: 0,
         }
     }
 
-    /// Given N -bytes input message, this routine consumes those into Keccak[256] sponge state
+    /// Given N -bytes input message, this routine consumes those into Keccak\[256\] sponge state
     ///
     /// Note, this routine can be called arbitrary number of times, each time with arbitrary
-    /// bytes of input message, until keccak[256] state is finalized ( by calling routine with
+    /// bytes of input message, until keccak\[256\] state is finalized ( by calling routine with
     /// similar name ). Once finalized, calling this routine again doesn't do anything.
     ///
     /// Adapted from https://github.com/itzmeanjan/sha3/blob/b5e897ed/include/shake128.hpp#L43-L130
@@ -92,12 +93,14 @@ impl TurboShake128 {
 
     /// After consuming N -bytes ( by invoking absorb routine arbitrary many times,
     /// each time with arbitrary input bytes ), this routine is invoked when no more
-    /// input bytes remaining to be consumed into Keccak[256] sponge state.
+    /// input bytes remaining to be consumed into Keccak\[256\] sponge state.
     ///
     /// Note, once this routine is called, calling absorb() or finalize() again, on same
     /// TurboSHAKE128 object doesn't do anything. After finalization, one might wish to
     /// read arbitrary many bytes by squeezing sponge, which is done by calling squeeze()
     /// function, as many times required.
+    ///
+    /// Adapted from https://github.com/itzmeanjan/sha3/blob/b5e897ed/include/shake128.hpp#L132-L192
     pub fn finalize<const D: u8>(&mut self) {
         if self.is_ready == usize::MAX {
             return;
@@ -117,5 +120,45 @@ impl TurboShake128 {
         self.offset = 0;
         self.is_ready = usize::MAX;
         self.squeezable = Self::RATE_BYTES;
+    }
+
+    /// Given that N -bytes input message is already absorbed into sponge state, this
+    /// routine is used for squeezing M -bytes out of consumable part of sponge state
+    /// ( i.e. rate portion of the state )
+    ///
+    /// Note, this routine can be called arbitrary number of times, for squeezing arbitrary
+    /// number of bytes from sponge Keccak\[256\].
+    ///
+    /// Make sure you absorb message bytes first, then only call this function, otherwise
+    /// it can't squeeze anything out.
+    ///
+    /// Adapted from https://github.com/itzmeanjan/sha3/blob/b5e897ed/include/shake128.hpp#L194-L238
+    pub fn squeeze(&mut self, out: &mut [u8]) {
+        if self.is_ready != usize::MAX {
+            return;
+        }
+
+        let olen = out.len();
+        let mut rate = [0u8; Self::RATE_BYTES];
+        let mut off = 0;
+
+        while off < olen {
+            let read = cmp::min(self.squeezable, olen - off);
+            let soff = Self::RATE_BYTES - self.squeezable;
+
+            for i in 0..Self::RATE_WORDS {
+                rate[i * 8..(i + 1) * 8].copy_from_slice(&self.state[i].to_le_bytes());
+            }
+
+            out[off..(off + read)].copy_from_slice(&rate[soff..(soff + read)]);
+
+            self.squeezable -= read;
+            off += read;
+
+            if self.squeezable == 0 {
+                keccak::permute(&mut self.state);
+                self.squeezable = Self::RATE_BYTES;
+            }
+        }
     }
 }
