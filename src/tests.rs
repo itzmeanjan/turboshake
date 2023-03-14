d07ef17abc2ec6e33e41279ee776169c1cbd265a
Anjan Roy <hello@itzmeanjan.in>
2023-03-14 19:07:28 +0400

    ensure functional correctness of TurboSHAKE256 XOF implementation
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,4 @@
-use crate::TurboShake128;
+use crate::{TurboShake128, TurboShake256};
 use std::cmp;
 
 /// Generates static byte pattern of length 251, following
@@ -27,6 +27,16 @@ fn ptn(n: usize) -> Vec<u8> {
 /// and returning TurboSHAKE128 object, ready to be squeezed.
 fn turboshake128<const D: u8>(msg: &[u8]) -> TurboShake128 {
     let mut hasher = TurboShake128::new();
+    hasher.absorb(msg);
+    hasher.finalize::<D>();
+    hasher
+}
+
+/// Given a message M of length n -bytes, absorbs it into TurboSHAKE256 object, while
+/// finalizing it by using domain seperator constant D ( generic constant parameter )
+/// and returning TurboSHAKE256 object, ready to be squeezed.
+fn turboshake256<const D: u8>(msg: &[u8]) -> TurboShake256 {
+    let mut hasher = TurboShake256::new();
     hasher.absorb(msg);
     hasher.finalize::<D>();
     hasher
@@ -154,11 +164,128 @@ fn test_turboshake128() {
         hex::encode(&out),
         "e4e1fd449c36ef25256c896e1907af3f458253d4a0bd820a6fef83377ae031f9"
     );
+}
 
+/// TurboSHAKE256 test vectors are collected from https://www.ietf.org/archive/id/draft-irtf-cfrg-kangarootwelve-09.html#name-test-vectors,
+/// based on reference implementation https://www.ietf.org/archive/id/draft-irtf-cfrg-kangarootwelve-09.html#name-pseudocode
+#[test]
+fn test_turboshake256() {
     let mut out = [0u8; 32];
-    turboshake128::<0x7f>(&[0u8; 0]).squeeze(&mut out);
+    turboshake256::<0x01>(&[0u8; 0]).squeeze(&mut out);
     assert_eq!(
         hex::encode(&out),
-        "e4e1fd449c36ef25256c896e1907af3f458253d4a0bd820a6fef83377ae031f9"
+        "e3dd2df0943bde6d82e39ec36059f35cd76720e2df38cc6b10b69fddfcaa3a4a"
+    );
+
+    let mut out = [0u8; 64];
+    turboshake256::<0x01>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "e3dd2df0943bde6d82e39ec36059f35cd76720e2df38cc6b10b69fddfcaa3a4a72fbbbe42c00ced7aa88e26d4675dd6e2c43c4413c4ea4d44bb170f03a981cab"
+    );
+
+    let mut out = [0u8; 10032];
+    turboshake256::<0x01>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out[10000..]),
+        "b021b244dcd9599966d7742225fc7372639233f0ff0863fa79683ebf1f57114f"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x01>(&ptn(17usize.pow(0))).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "73ebf1d543d855a3c5e4be6322f75604c254f70394b396884b6010fcca694722"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x01>(&ptn(17usize.pow(1))).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "1da47d188755b75307a242a8f2675bbd76aebf8a13b1d40f587a0732cbb3dc3d"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x01>(&ptn(17usize.pow(2))).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "a48c938770f916b09d764e29e2279b90d5fa3dd0e006ee8d6c2eb0db8893525e"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x01>(&ptn(17usize.pow(3))).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "75e8668d3a46baa7c75c3ac7d33fc2c218df38cdf0f8d70352a495bd9d5d6dfa"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x01>(&ptn(17usize.pow(4))).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "ffa49653e40c7ba33f11c278d99be3010f65446a7bf8a69d70b07feb54e7107c"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x01>(&ptn(17usize.pow(5))).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "2ad2b3beb8671840fa9d5e8f7faf2d1139d99483f3c4e56a6a25553f83c25931"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x02>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "cfdbc69ec2652711dc3013cee68def374948ef09e62d82f2749e3dbc71f04dce"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x03>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "1402a1d6bebcf52cdbc7074c3d7b1adc545646458400a63980ebb3dd0ab04c68"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x0c>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "3c78a84557f19506a6151985664cf6163c4d4033d6bc310f8e8dde56e232abf4"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x23>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "bd8f3f5eae3fb4ba604ad2d9d9431867532ab1e2f773819620b79281e3258bbc"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x3a>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "cfa491078479604fd78e967071a081cf357a1244d2999c929c318782a24d7c21"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x51>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "b92a11dd21017255a8285bbdf413269dcfae55f79d188a55cc2e04ea667bc047"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x68>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "229acb8530b6e700bebb304655a5dfad00f7ac4ab7f582ee909c11b96f6d5fb3"
+    );
+
+    let mut out = [0u8; 32];
+    turboshake256::<0x7f>(&[0u8; 0]).squeeze(&mut out);
+    assert_eq!(
+        hex::encode(&out),
+        "49b38a11204328440c4c40fdaee305629379936d7a31f9474c4f0fb062a2a427"
     );
 }
