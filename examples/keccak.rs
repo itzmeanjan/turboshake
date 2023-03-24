80042198e1d2ef03786fcdf9e94875cd6dc216b9
Anjan Roy <hello@itzmeanjan.in>
2023-03-24 12:30:44 +0400

    add new example showing usage of 4x parallel SIMD keccak-p[1600,12] permutation
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- examples/keccak.rs
+++ examples/keccak.rs
@@ -3,7 +3,8 @@ use turboshake::keccak;
 fn main() {
     let mut state = [1u64; 25];
 
-    println!("[before] state = {:?}", state);
+    // Apply keccak-p[1600, 12] permutation
     keccak::permute(&mut state);
+
     println!("[after] state = {:?}", state);
 }
