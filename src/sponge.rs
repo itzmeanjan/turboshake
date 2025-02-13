6205b3785806aea158ba703b2eab4be3f52c020e
Anjan Roy <hello@itzmeanjan.in>
2025-02-13 20:21:27 +0400

    Refactor sponge squeeze function
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/sponge.rs
+++ src/sponge.rs
@@ -88,68 +88,36 @@ pub fn squeeze<const RATE_BYTES: usize, const RATE_WORDS: usize>(
     readable: &mut usize,
     out: &mut [u8],
 ) {
-    let olen = out.len();
-    let mut rate = [0u8; RATE_BYTES];
-    let mut off = 0;
+    const { assert!(RATE_BYTES % KECCAK_WORD_BYTE_LEN == 0) }
 
-    while off < olen {
-        let read = cmp::min(*readable, olen - off);
-        let soff = RATE_BYTES - *readable;
+    let mut block = [0u8; RATE_BYTES];
+    let mut out_offset = 0;
 
-        for i in 0..RATE_WORDS {
-            rate[i * 8..(i + 1) * 8].copy_from_slice(&state[i].to_le_bytes());
-        }
+    while out_offset < out.len() {
+        let state_byte_offset = RATE_BYTES - *readable;
+        let remaining_num_bytes = out.len() - out_offset;
+        let squeezable_num_bytes = min(remaining_num_bytes, *readable);
+        let effective_block_byte_len = state_byte_offset + squeezable_num_bytes;
+        let padded_efffective_block_len = (effective_block_byte_len + (KECCAK_WORD_BYTE_LEN - 1))
+            & KECCAK_WORD_BYTE_LEN.wrapping_neg();
 
-        out[off..(off + read)].copy_from_slice(&rate[soff..(soff + read)]);
+        let mut state_word_index = 0;
+        block[..padded_efffective_block_len]
+            .chunks_exact_mut(KECCAK_WORD_BYTE_LEN)
+            .for_each(|chunk_bytes| {
+                chunk_bytes.copy_from_slice(&state[state_word_index].to_le_bytes());
+                state_word_index += 1;
+            });
 
-        *readable -= read;
-        off += read;
+        out[out_offset..(out_offset + squeezable_num_bytes)]
+            .copy_from_slice(&block[state_byte_offset..(state_byte_offset + squeezable_num_bytes)]);
+
+        *readable -= squeezable_num_bytes;
+        out_offset += squeezable_num_bytes;
 
         if *readable == 0 {
             keccak::permute(state);
             *readable = RATE_BYTES;
         }
-    }
-}
-
-/// Converts a slice of bytes into a u64 in little-endian byte order.
-///
-/// Reads at most 8 bytes from the input slice. If the slice is shorter than 8 bytes, it reads only the available bytes,
-/// while setting other bytes to 0. The function handles cases where the input slice is empty.
-///
-/// # Arguments
-///
-/// * `bytes` - The slice of bytes to convert.
-///
-/// # Returns
-///
-/// A u64 representing the bytes in little-endian byte order.
-#[inline(always)]
-pub fn u64_from_le_bytes(bytes: &[u8]) -> u64 {
-    let mut word = 0;
-    let readable_num_bytes = min(bytes.len(), std::mem::size_of::<u64>());
-
-    for (idx, &byte) in bytes.iter().enumerate().take(readable_num_bytes) {
-        word |= (byte as u64) << (idx * 8);
-    }
-
-    word
-}
-
-/// Converts a u64 into a slice of bytes in little-endian byte order.
-///
-/// Writes at most 8 bytes to the output slice. If the slice is shorter than 8 bytes, it writes only the those many bytes.
-/// The function handles cases where the output slice is empty.
-///
-/// # Arguments
-///
-/// * `word` - The u64 to convert.
-/// * `bytes` - The mutable slice of bytes to write to.
-#[inline(always)]
-pub fn u64_to_le_bytes(word: u64, bytes: &mut [u8]) {
-    let writable_num_bytes = min(bytes.len(), std::mem::size_of::<u64>());
-
-    for (idx, byte) in bytes.iter_mut().enumerate().take(writable_num_bytes) {
-        *byte = (word >> (idx * 8)) as u8;
     }
 }
