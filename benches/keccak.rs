c97d586d778c9ed39331fb129c027ba8ae9956df
Anjan Roy <hello@itzmeanjan.in>
2025-02-13 21:06:35 +0400

    Refactor keccak permutation benchmark using `divan`
    More about divan @ https://nikolaivazquez.com/blog/divan/
    
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benches/keccak.rs
+++ benches/keccak.rs
@@ -1,71 +1,26 @@
-use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion, Throughput};
-use rand::{thread_rng, Rng};
+use divan;
+use divan::counter::{BytesCount, BytesFormat, ItemsCount};
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
 use turboshake::keccak;
 
-#[cfg(any(
-    target_arch = "x86_64",
-    target_arch = "x86",
-    target_arch = "aarch64",
-    target_arch = "loongarch64"
-))]
-use criterion_cycles_per_byte::CyclesPerByte;
-
-#[cfg(any(
-    target_arch = "x86_64",
-    target_arch = "x86",
-    target_arch = "aarch64",
-    target_arch = "loongarch64"
-))]
-type CriterionHandler = Criterion<CyclesPerByte>;
-
-#[cfg(not(any(
-    target_arch = "x86_64",
-    target_arch = "x86",
-    target_arch = "aarch64",
-    target_arch = "loongarch64"
-)))]
-type CriterionHandler = Criterion;
-
-fn keccak(c: &mut CriterionHandler) {
-    let mut rng = thread_rng();
-
-    let mut group = c.benchmark_group("keccak");
-    group.throughput(Throughput::Bytes(200u64)); // size of keccak-p[1600] permutation state
-
-    group.bench_function("keccak-p[1600, 12] (cached)", |bench| {
-        let mut state = [0u64; 25];
-        rng.fill(&mut state);
-
-        bench.iter(|| keccak::permute(black_box(&mut state)))
-    });
-    group.bench_function("keccak-p[1600, 12] (random)", |bench| {
-        let mut state = [0u64; 25];
-        rng.fill(&mut state);
-
-        bench.iter_batched(
-            || state.clone(),
-            |mut state| keccak::permute(black_box(&mut state)),
-            BatchSize::SmallInput,
-        )
-    });
-
-    group.finish();
+fn main() {
+    divan::Divan::default()
+        .bytes_format(BytesFormat::Binary)
+        .run_benches();
 }
 
-#[cfg(any(
-    target_arch = "x86_64",
-    target_arch = "x86",
-    target_arch = "aarch64",
-    target_arch = "loongarch64"
-))]
-criterion_group!(name = permutation; config = Criterion::default().with_measurement(CyclesPerByte); targets = keccak);
+#[divan::bench]
+fn permute_12_rounds(bencher: divan::Bencher) {
+    let mut rng = ChaCha8Rng::from_os_rng();
 
-#[cfg(not(any(
-    target_arch = "x86_64",
-    target_arch = "x86",
-    target_arch = "aarch64",
-    target_arch = "loongarch64"
-)))]
-criterion_group!(permutation, keccak);
+    let mut state = [0u64; 25];
+    let state_byte_len = state.len() * 8;
+    rng.fill(&mut state);
 
-criterion_main!(permutation);
+    bencher
+        .counter(BytesCount::new(state_byte_len))
+        .counter(ItemsCount::new(1usize))
+        .with_inputs(|| (state.clone()))
+        .bench_values(|mut state| keccak::permute(divan::black_box(&mut state)));
+}
